66ce2fb539e0549a9a013fdb4f2d87bf436667da
michellemariedd <66342399+michellemariedd@users.noreply.github.com>
2020-06-13 20:06:09 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HangManGame
+{
+    partial class HangManGame
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxCategorias = new System.Windows.Forms.ComboBox();
+            this.lbMensaje = new System.Windows.Forms.Label();
+            this.flFichas = new System.Windows.Forms.FlowLayoutPanel();
+            this.flPalabra = new System.Windows.Forms.FlowLayoutPanel();
+            this.intento = new System.Windows.Forms.PictureBox();
+            this.btnIniciarJuego = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.intento)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxCategorias
+            // 
+            this.cbxCategorias.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.cbxCategorias.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxCategorias.FormattingEnabled = true;
+            this.cbxCategorias.Location = new System.Drawing.Point(24, 102);
+            this.cbxCategorias.Name = "cbxCategorias";
+            this.cbxCategorias.Size = new System.Drawing.Size(259, 51);
+            this.cbxCategorias.TabIndex = 1;
+            this.cbxCategorias.Text = "Categorias";
+            this.cbxCategorias.SelectedIndexChanged += new System.EventHandler(this.cbxCategorias_SelectedIndexChanged);
+            // 
+            // lbMensaje
+            // 
+            this.lbMensaje.AutoSize = true;
+            this.lbMensaje.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMensaje.Location = new System.Drawing.Point(553, 38);
+            this.lbMensaje.Name = "lbMensaje";
+            this.lbMensaje.Size = new System.Drawing.Size(203, 43);
+            this.lbMensaje.TabIndex = 2;
+            this.lbMensaje.Text = "!Tú lo mataste!";
+            // 
+            // flFichas
+            // 
+            this.flFichas.Location = new System.Drawing.Point(21, 173);
+            this.flFichas.Name = "flFichas";
+            this.flFichas.Size = new System.Drawing.Size(477, 218);
+            this.flFichas.TabIndex = 4;
+            // 
+            // flPalabra
+            // 
+            this.flPalabra.Location = new System.Drawing.Point(24, 406);
+            this.flPalabra.Name = "flPalabra";
+            this.flPalabra.Size = new System.Drawing.Size(474, 83);
+            this.flPalabra.TabIndex = 5;
+            this.flPalabra.Paint += new System.Windows.Forms.PaintEventHandler(this.flPalabra_Paint);
+            // 
+            // intento
+            // 
+            this.intento.Image = global::HangManGame.Properties.Resources.Intento;
+            this.intento.Location = new System.Drawing.Point(536, 102);
+            this.intento.Name = "intento";
+            this.intento.Size = new System.Drawing.Size(252, 423);
+            this.intento.TabIndex = 3;
+            this.intento.TabStop = false;
+            // 
+            // btnIniciarJuego
+            // 
+            this.btnIniciarJuego.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciarJuego.Image = global::HangManGame.Properties.Resources.Jugando;
+            this.btnIniciarJuego.Location = new System.Drawing.Point(12, 12);
+            this.btnIniciarJuego.Name = "btnIniciarJuego";
+            this.btnIniciarJuego.Size = new System.Drawing.Size(344, 69);
+            this.btnIniciarJuego.TabIndex = 0;
+            this.btnIniciarJuego.UseVisualStyleBackColor = true;
+            this.btnIniciarJuego.Click += new System.EventHandler(this.btnIniciarJuego_Click_1);
+            // 
+            // HangManGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.flPalabra);
+            this.Controls.Add(this.flFichas);
+            this.Controls.Add(this.intento);
+            this.Controls.Add(this.lbMensaje);
+            this.Controls.Add(this.cbxCategorias);
+            this.Controls.Add(this.btnIniciarJuego);
+            this.Name = "HangManGame";
+            this.Text = "HangMan";
+            this.Load += new System.EventHandler(this.HangManGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.intento)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciarJuego;
+        private System.Windows.Forms.ComboBox cbxCategorias;
+        private System.Windows.Forms.Label lbMensaje;
+        private System.Windows.Forms.PictureBox intento;
+        private System.Windows.Forms.FlowLayoutPanel flFichas;
+        private System.Windows.Forms.FlowLayoutPanel flPalabra;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using HangManGame.CusomItem;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HangManGame
+{
+    public partial class HangManGame : Form
+    {
+        private bool isLoad { get; set; } = true;
+
+        char[] PalabrasAdivinadas; int oportunidades;
+        char[] PalabrasSeleccionada;
+        char[] Alfabeto;
+       
+        string[] Animales;
+        string[] Frutas;
+        string[] AutosBrands;
+
+        int option = 0;
+
+        public HangManGame()
+        {
+            
+            InitializeComponent();
+        }
+
+        public void IniciarJuego(int categoria)
+        {
+            Random random = new Random();
+            int IndicePalabrasSeleccionada = 0;
+            flFichas.Controls.Clear();
+            flFichas.Enabled = true;
+            intento.Image = null;
+            lbMensaje.Visible = false;
+            oportunidades = 0; 
+            btnIniciarJuego.Image = Properties.Resources.Jugando;
+
+             Animales = new string[] {  "Perro", "Gato", "Elefante", "Vaca", "Burro", "Zorro", "Oso", "cebra", "Leon", "Mono" };
+             Frutas = new string[] {"Uva", "Manzana", "Banana", "Pera", "Lechoza", "Fresa", "Mango", "Sandia", "Chinola", "Melon" };
+             AutosBrands = new string[] { "Audi", "Corolla", "Honda", "Kia", "infiniti", "Mazda", "Nissan", "Ford", "Jeep", "Lexus" };
+
+            Alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+           if (categoria == 1)
+            {
+                IndicePalabrasSeleccionada = random.Next(0, Animales.Length);
+                PalabrasSeleccionada = Animales[IndicePalabrasSeleccionada].ToUpper().ToCharArray();
+                PalabrasAdivinadas = PalabrasSeleccionada;
+            }
+           if (categoria == 2)
+            {
+                IndicePalabrasSeleccionada = random.Next(0, Frutas.Length);
+                PalabrasSeleccionada = Frutas[IndicePalabrasSeleccionada].ToUpper().ToCharArray();
+                PalabrasAdivinadas = PalabrasSeleccionada;
+            }
+           if (categoria == 3)
+            {
+                IndicePalabrasSeleccionada = random.Next(0, AutosBrands.Length);
+                PalabrasSeleccionada = AutosBrands[IndicePalabrasSeleccionada].ToUpper().ToCharArray();
+                PalabrasAdivinadas = PalabrasSeleccionada;
+            }
+            
+            
+           
+
+            foreach (char LetraAlfabeto in Alfabeto)
+            {
+                Button btnLetra = new Button();
+                btnLetra.Tag = "";
+                btnLetra.Text = LetraAlfabeto.ToString();
+                btnLetra.Width = 60;
+                btnLetra.Height = 40;
+                btnLetra.Click += Compara;
+                btnLetra.ForeColor = Color.Transparent;
+                btnLetra.FlatStyle = FlatStyle.Standard;
+                btnLetra.Font = new Font(btnLetra.Font.Name, 15, FontStyle.Bold);
+                btnLetra.BackgroundImageLayout = ImageLayout.Center;
+                btnLetra.BackColor = Color.Black;
+                btnLetra.Name = LetraAlfabeto.ToString();
+                flFichas.Controls.Add(btnLetra);
+            }
+
+            flPalabra.Controls.Clear();
+           
+            for (int IndiceValorLetra = 0; IndiceValorLetra < PalabrasSeleccionada.Length; IndiceValorLetra++)
+            {
+                
+                Button Letra = new Button();
+                Letra.Tag = PalabrasSeleccionada[IndiceValorLetra].ToString();
+                Letra.Text = "-";
+                Letra.Width = 46;
+                Letra.Height = 60;
+                Letra.ForeColor = Color.Transparent;
+                Letra.FlatStyle = FlatStyle.Standard;
+                Letra.Font = new Font(Letra.Font.Name, 15, FontStyle.Bold);
+                Letra.BackgroundImageLayout = ImageLayout.Center;
+                Letra.BackColor = Color.Black;
+                Letra.Name = "Adivinado" + IndiceValorLetra.ToString();
+                flPalabra.Controls.Add(Letra);
+            }
+           
+        }
+         void Compara(object sender, EventArgs e)
+        {
+            bool encontrado = false;
+            Button btnComparar = (Button)sender;
+            btnComparar.BackColor = Color.Transparent;
+            btnComparar.ForeColor = Color.Black;
+            btnComparar.Enabled = false;
+
+            for (int indiceReview = 0; indiceReview < PalabrasAdivinadas.Length; indiceReview++)
+            {
+                if (PalabrasAdivinadas[indiceReview] == char.Parse(btnComparar.Text))
+                {
+                    Button tbx = this.Controls.Find("Adivinado" + indiceReview, true).FirstOrDefault() as Button;
+                    tbx.Text = PalabrasAdivinadas[indiceReview].ToString();
+                    PalabrasAdivinadas[indiceReview] = '-';
+                    encontrado = true;
+                }
+            }
+            
+            bool Ganaste = true;
+
+            for (int indiceGanador = 0; indiceGanador < PalabrasAdivinadas.Length; indiceGanador++)
+            {
+                if (PalabrasAdivinadas[indiceGanador] != '-')
+                {
+                    Ganaste = false;
+                }
+            }
+
+            if (Ganaste)
+            {
+                MessageBox.Show("Ganaste!!"); btnIniciarJuego.Image = Properties.Resources.btnStart;
+            }
+
+            if (!encontrado)
+            {
+                oportunidades = oportunidades + 1;
+                intento.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject("intento" + oportunidades);
+
+            }
+
+            if (oportunidades == 5)
+            {
+                lbMensaje.Visible = true;
+
+                for (int IndiceValorLetra = 0; IndiceValorLetra < PalabrasSeleccionada.Length; IndiceValorLetra++)
+                {
+
+                    Button btnLetra = this.Controls.Find("Adivinado" + IndiceValorLetra, true).FirstOrDefault() as Button;
+                    btnLetra.Text = btnLetra.Tag.ToString();
+
+                }
+
+                flFichas.Enabled = false;
+                btnIniciarJuego.Image = Properties.Resources.btnStart;
+            }
+         }
+         
+        private void LoadComboBox()
+        {
+            ComboBoxItem animalesOpcion = new ComboBoxItem
+            {
+                Text = "Animales",
+                Value = 1
+            };
+            ComboBoxItem frutasOpcion = new ComboBoxItem
+            {
+                Text = "Frutas",
+                Value = 2
+            };
+            ComboBoxItem autosbrandOpcion = new ComboBoxItem
+            {
+                Text = "AutoBrand",
+                Value = 3
+            };
+           
+            cbxCategorias.Items.Add(animalesOpcion);
+            cbxCategorias.Items.Add(frutasOpcion);
+            cbxCategorias.Items.Add(autosbrandOpcion);
+        }
+
+        private void flPalabra_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void HangManGame_Load(object sender, EventArgs e)
+        {
+            LoadComboBox();
+            isLoad = false;
+        }
+
+        private void Seleccion() 
+        {
+            ComboBoxItem seleccionado = cbxCategorias.SelectedItem as ComboBoxItem;
+
+            if (seleccionado.Value == null)
+            {
+                MessageBox.Show("Debe seleccionar una opcion de juego", "Advertencia");
+            }
+          
+            if (Convert.ToInt32(seleccionado.Value) == 1)
+            {
+                option = 1;
+            }
+            if (Convert.ToInt32(seleccionado.Value) == 2)
+            {
+                option = 2;
+            }
+             if (Convert.ToInt32(seleccionado.Value) == 3)
+             {
+                option = 3;
+             }
+        }
+
+        private void btnIniciarJuego_Click_1(object sender, EventArgs e)
+        {
+            IniciarJuego(option);
+        }
+
+        private void cbxCategorias_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+            Seleccion();
+        }
+    }
+}
